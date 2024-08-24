b167908f0529e165494d1afd9ae7b889c45a722d
CinthiaR13986 <crobless@miumg.edu.gt>
2024-08-23 21:10:45 -0600

    Add files via upload

---
+++ Vehiculo.java
@@ -0,0 +1,14 @@
+package com.herencia.main;
+
+public class Vehiculo {
+
+	public Vehiculo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
